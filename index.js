e5c861f81f567223b8a224e4e2f66d1e7878f289
anggris <ferdinandanggris@gmail.com>
2022-06-27 23:11:14 +0700

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest : './uploads'})
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 var app = express();
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +21,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/',function(req,res){
+  
+});
+
+app.post('/api/fileanalyse/',upload.single('upfile'),function(req,res){
+  res.send({name : req.file.originalname,type : req.file.mimetype,size: req.file.size});
+})
 
 
 
